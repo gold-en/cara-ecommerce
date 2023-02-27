ca66689e87de8c1a339f23520be10e136a255e06
Golden1636 <okeamagolden@yahoo.com>
2023-02-27 14:34:00 +0100

    landing page is now responsive to medium and mobile screens

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+'use strict';
+const hamburger = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
 
+console.log(bar);
+
+if (hamburger) {
+  // if the hamburger is showing on the screen then... add an eventListener to it
+  hamburger.addEventListener('click', () => {
+    nav.classList.add('active');
+  });
+}
+
+if (close) {
+  // if the close(X) is showing on the screen then... add an eventListener to it
+  close.addEventListener('click', () => {
+    nav.classList.remove('active');
+  });
+}
